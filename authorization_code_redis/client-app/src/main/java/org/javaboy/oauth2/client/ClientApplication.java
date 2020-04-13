defe50346aa42d52c69b1911500124a34521b546
江南一点雨 <wangsong0210@gmail.com>
2020-04-14 00:11:32 +0800

    m

---
+++ authorization_code_redis/client-app/src/main/java/org/javaboy/oauth2/client/ClientApplication.java
@@ -0,0 +1,19 @@
+package org.javaboy.oauth2.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
