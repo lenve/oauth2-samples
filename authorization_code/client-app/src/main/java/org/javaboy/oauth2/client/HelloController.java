ef38ca15e6b9e50a7716bfcc832a71d4e4b3cafa
江南一点雨 <wangsong0210@gmail.com>
2020-04-13 13:18:19 +0800

    m

---
+++ authorization_code/client-app/src/main/java/org/javaboy/oauth2/client/HelloController.java
@@ -0,0 +1,53 @@
+package org.javaboy.oauth2.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+/**
+ * @作者 江南一点雨
+ * @微信公众号 江南一点雨
+ * @网站 http://www.itboyhub.com
+ * @国际站 http://www.javaboy.org
+ * @微信 a_java_boy
+ * @GitHub https://github.com/lenve
+ * @Gitee https://gitee.com/lenve
+ */
+@Controller
+public class HelloController {
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/index.html")
+    public String hello(String code, Model model) {
+        if (code != null) {
+            MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+            map.add("code", code);
+            map.add("client_id", "javaboy");
+            map.add("client_secret", "123");
+            map.add("redirect_uri", "http://localhost:8082/index.html");
+            map.add("grant_type", "authorization_code");
+            Map<String,String> resp = restTemplate.postForObject("http://localhost:8080/oauth/token", map, Map.class);
+            String access_token = resp.get("access_token");
+            System.out.println(access_token);
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", "Bearer " + access_token);
+            HttpEntity<Object> httpEntity = new HttpEntity<>(headers);
+            ResponseEntity<String> entity = restTemplate.exchange("http://localhost:8081/admin/hello", HttpMethod.GET, httpEntity, String.class);
+            model.addAttribute("msg", entity.getBody());
+        }
+        return "index";
+    }
+}
