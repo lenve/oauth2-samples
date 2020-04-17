b4092ec21b04531ed57fc93207f7bd80a5c70dc9
江南一点雨 <wangsong0210@gmail.com>
2020-04-17 09:34:23 +0800

    m

---
+++ authorization_code_redis/auth-server/src/main/java/org/javaboy/oauth2/auth/AccessTokenConfig.java
@@ -0,0 +1,28 @@
+package org.javaboy.oauth2.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
+/**
+ * @作者 江南一点雨
+ * @微信公众号 江南一点雨
+ * @网站 http://www.itboyhub.com
+ * @国际站 http://www.javaboy.org
+ * @微信 a_java_boy
+ * @GitHub https://github.com/lenve
+ * @Gitee https://gitee.com/lenve
+ */
+@Configuration
+public class AccessTokenConfig {
+    @Autowired
+    RedisConnectionFactory redisConnectionFactory;
+    @Bean
+    TokenStore tokenStore() {
+        return new RedisTokenStore(redisConnectionFactory);
+    }
+}
---
+++ authorization_code_redis/client-app/src/main/java/org/javaboy/oauth2/client/HelloController.java
@@ -0,0 +1,37 @@
+package org.javaboy.oauth2.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+/**
+ * @作者 江南一点雨
+ * @微信公众号 江南一点雨
+ * @网站 http://www.itboyhub.com
+ * @国际站 http://www.javaboy.org
+ * @微信 a_java_boy
+ * @GitHub https://github.com/lenve
+ * @Gitee https://gitee.com/lenve
+ */
+@Controller
+public class HelloController {
+    @Autowired
+    TokenTask tokenTask;
+    @GetMapping("/index.html")
+    public String hello(String code, Model model) {
+        model.addAttribute("msg", tokenTask.getData(code));
+        return "index";
+    }
+}
